372791a42e452427b424c7c42a5ebd57ac9bbb30
nelgamal <znhg0906@leeds.ac.uk>
2026-02-27 02:19:20 +0000

    End of Portfolio!

--- worksheet/task2/task2.c
+++ worksheet/task2/task2.c
@@ -11,30 +11,29 @@
 
 int main(void){
 	long decimal=0;
-	char hex[9];
+	char hex[9]; // max length is 8 character and there is one added for the terminator ("\0")
 	int i;
-	int digit;
+	int digit; // the value of each hexadecimal digit from 0-15
 	
 	printf("Enter a hexadecimal:");
-	scanf("%8s", hex);
+	scanf("%8s", hex); // ensures that no more than 8 characters are read - hence preventing overflow 
 
+	// the for loop allows the program through each character in the string 
 	for (i=0; i<strlen(hex); i++){
-		char character = hex[i];
+		char character = hex[i]; // the current hex digit
 		
 		if (character >= '0' && character <= '9') {
-			digit = character - '0';
+			digit = character - '0'; // 0-9 remains 0-9 
 		} else if(character >= 'a' && character <= 'f'){
-			digit = character - 'a' + 10;
+			digit = character - 'a' + 10; // a-f changes to 10-15
 		} else if(character >= 'A' && character <= 'F') {
-			digit = character - 'A' + 10;
+			digit = character - 'A' + 10; // A-F chnaged to 10-15 
 		} else {
-			printf("Error: Invalid Hexadecimal\n");
+			printf("Error: Invalid Hexadecimal\n"); // if input contains invalid hex digit
 			return 1;
 		}
-		decimal = decimal * 16 + digit;
+		decimal = decimal * 16 + digit; // multiplying the current number by 16 and adds the new digit to build the final value 
 	}
-	// if input contains invalid hex digit
-	// printf("Error: Invalid Hexadecimal\n");
 	
 	// print the decimal result
 	printf("decimal:%ld\n", decimal);
